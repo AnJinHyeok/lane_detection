d4f071a707c86f28e956c3e5d9b54fdc22534662
NathanKys <64261642+NathanKys@users.noreply.github.com>
2020-07-20 16:12:12 +0900

    Update 1_hough_transform.py

--- 1_hough_transform.py
+++ 1_hough_transform.py
@@ -14,7 +14,7 @@ for line in hough_line:                                                 # 여기
     x1, y1, x2, y2 = line[0]                                            # 좌표 = line[0] 해도되고 line.reshape(4) 해도 됨. 이건 넘겨받은 좌표값들 2차원 -> 1차원 배열로 차원 내려주는것
     cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 10)                # cv.line은 라인그려주는 함수. x와y좌표 순서대로 주고 색생과 선 굵기 정해줌
     
-    # xi yi x2 y2 는 for문 안에서만 살아있음!!!!!!!! cv.line 꼭 for문 안에 설정해주기!!!!!!!
+    # x1 y1 x2 y2 는 for문 안에서만 살아있음!!!!!!!! cv.line 꼭 for문 안에 설정해주기!!!!!!!
     # 전역변수인 img를 받아와서 cv.line을 for문 안에서 처리해줘도 적용 됨.
 
 cv2.imshow("result", image)
